6ac0675bf65662e38f1713112d1c093c4e0753d5
edward_martin <32644669+em1195@users.noreply.github.com>
2019-04-29 15:30:25 +0100

    added data generating scripts

---
+++ database/README.md
@@ -0,0 +1,3 @@
+# proj_git
+
+Here is the github page containing my third year project, in which i am building the Graph Convolution Network outlined in this paper:
---
+++ database/calcProperty.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append(r'/home/edward/anaconda3/rdkit')
+
+import numpy as np
+import rdkit
+
+from rdkit import Chem
+from rdkit.Chem.QED import qed
+from rdkit.Chem.Descriptors import ExactMolWt
+from rdkit.Chem.Crippen import MolLogP
+from rdkit.Chem.rdMolDescriptors import CalcTPSA
+from rdkit.Chem import RDConfig
+import os
+import sys
+sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
+# now you can import sascore!
+import sascorer
+from sascorer import calculateScore
+
+
+
+
+smiles_f = open('./ZINC/smiles.txt')
+smiles_list = smiles_f.readlines()
+
+logPList = []
+molWtList = []
+TPSAList = []
+QEDList = []
+SASList = []
+for smi in smiles_list:
+    smi = smi.strip()
+    m = Chem.MolFromSmiles(smi)
+    molWt = ExactMolWt(m)
+    logP = MolLogP(m)
+    TPSA = CalcTPSA(m)
+    _qed = qed(m)
+    sas = calculateScore(m)
+
+    logPList.append(logP)
+    molWtList.append(molWt)
+    TPSAList.append(TPSA)
+    QEDList.append(_qed)
+    SASList.append(sas)
+
+logPList = np.asarray(logPList)
+TPSAList = np.asarray(TPSAList)
+QEDList = np.asarray(QEDList)
+SASList = np.asarray(SASList)
+
+np.save('./ZINC/logP.npy', logPList)
+np.save('./ZINC/TPSA.npy', TPSAList)
+np.save('./ZINC/QED.npy', QEDList)
+np.save('./ZINC/SAS.npy', SASList)
